c44a866d598fa47017322325756338e0851737f1
Hector Vela <hector.vela@wizeline.com>
2018-03-23 13:26:36 -0600

    create route for single movie

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import VueRouter from 'vue-router'
 
 import TodoPage from '../components/todo/TodoPage'
 import MoviePage from '../components/movies/MoviePage'
+import MovieSingle from '../components/movies/MovieSingle'
 
 Vue.use(VueRouter)
 
 const routes = [
   {path: '/', component: TodoPage},
   {path: '/movies', component: MoviePage},
-  {path: '/movies/backlog', component: MoviePage}
+  {path: '/movies/backlog', component: MoviePage},
+  {path: '/movies/:movie_id', name: 'movieDetail', component: MovieSingle},
 ]
 
 export default new VueRouter({
